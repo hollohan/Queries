7b2b6860ce201df9011109147285a830b3e47b25
Ubuntu <ubuntu@ip-172-31-27-149.ec2.internal>
2020-01-10 23:57:52 +0000

    202001101859

--- Queries.py
+++ Queries.py
@@ -1,45 +1,74 @@
 import pymysql.cursors
 
 class Queries():
-	def __init__(self, db, host='localhost', user='root', password='toor'):
+	def __init__(self, db='log', host='localhost', user='grower', password='grower'):
 		self.conn = pymysql.connect(
-			host = 'localhost',
-			user = 'root',
-			password = 'toor',
-			db = db)        
+			host = host,
+			user = user,
+			password = password,
+			db = db)
 		self.cursor = self.conn.cursor()
+
+	def reconnect(self):
+		self.close()
+		self.__init__()
 
 	def run_query(self, q_string):
 		'''
 			executes q_string in DB assigned to self
 		'''
-		conn = self.conn
-		cursor = self.cursor
-		cursor.execute(q_string)
-		results = self.cursor.fetchall()
-		conn.commit()
+
+		while True:
+			try:
+				conn = self.conn
+				cursor = self.cursor
+				cursor.execute(q_string)
+				results = self.cursor.fetchall()
+				conn.commit()
+				break
+			except Exception as e:
+				print('[Queries.py][run_)query()] ERROR->{}'.format(e))
+				reconnect()
 
 		return results
-		
+
 	def close(self): self.conn.close()
-	
+
+	def delete (self, frm, whr=0):
+		q_string = 'DELETE FROM {}'.format(frm)
+
+
+		if whr:
+			criteria = []
+			for c in whr:
+				if type(whr[c]) is int: # check if into for correct SQL formatting
+					criteria.append('%s=%s' % (c, whr[c]))
+				else: # if not int is string
+					criteria.append('%s="%s"'  % (c, whr[c]))
+			criteria = ' AND '.join(criteria)
+			q_string += ' WHERE ' + criteria
+
+		print ('[executing] ' + q_string)
+		results = self.run_query(q_string)
+		return results
+
 	def select(self, fields, frm, whr=0):
 		'''
-			executes select query			
-			fields = fields to return, ['first','last','etc',...] 
+			executes select query
+			fields = fields to return, ['first','last','etc',...]
 			frm = table to select from 'tableA'
 			whr = criteria {'id':25,'username':'hollohae'}
 			! does not accept *, all field names must be chosen explicitly
-			
+
 		'''
 		q_string = 'SELECT '
 		# add fields to q_string
 		fields_str = ','.join(fields) # format list into comma seperated string
 		q_string += fields_str + ' '
-		
+
 		# add table to q_string
 		q_string += 'FROM ' + frm
-		
+
 		# add criteria to q_string
 		if whr:
 			criteria = []
@@ -50,8 +79,8 @@ class Queries():
 					criteria.append('%s="%s"'  % (c, whr[c]))
 			criteria = ' AND '.join(criteria)
 			q_string += ' WHERE ' + criteria
-		
-		print '[executing] ' + q_string
+
+		print ('[executing] ' + q_string)
 		results = self.run_query(q_string)
 		results = self.objectize(results, fields)
 		return results
@@ -68,17 +97,17 @@ class Queries():
 			for i in range(len(fields)):
 				newObj[fields[i]] = result[i]
 			newResults.append(newObj)
-				
+
 		return newResults
-		
+
 	def insert(self, table, values):
 		'''
 			executes insert into statement
 			table = str, table to insert into
 			valus = obj, fields and values to inser, {'first':'eric','last':'hollohan'}
 		'''
-		q_string = 'INSERT INTO  '
-		
+		q_string = 'INSERT INTO '
+
 		# add table to q_string
 		q_string += table + ' '
 
@@ -88,10 +117,12 @@ class Queries():
 		f = ','.join(f)
 		v = ','.join(v)
 		q_string += '(%s) VALUES (%s)' % (f, v)
-		
-		print '[executing] ' + q_string
+
+		print ('[executing] ' + q_string)
 		self.run_query(q_string)
-	
+		r = self.run_query('SELECT LAST_INSERT_ID()')
+		return (r[0][0])
+
 	def update(self, table, values, whr=0):	
 		'''
 			updates table with values where criteria
@@ -100,15 +131,15 @@ class Queries():
 			whr = {field:val}, match criteria, where ...
 		'''
 		q_string = 'UPDATE '
-		
+
 		# add table
 		q_string += table + ' '
-		
+
 		# add values
 		q_string += 'SET '
 		values = ['%s=%i' % (x, values[x]) if type(values[x]) is int else '%s="%s"' % (x, values[x]) for x in values]
 		q_string += ','.join(values)
-		
+
 		# add criteria
 		if whr:
 			criteria = []
@@ -119,10 +150,10 @@ class Queries():
 					criteria.append('%s="%s"'  % (c, whr[c]))
 			criteria = ' AND '.join(criteria)
 			q_string += ' WHERE ' + criteria
-		
-		print '[executing] ' + q_string
+
+		print ('[executing] ' + q_string)
 		self.run_query(q_string)
-			
+
 if __name__=='__main__':
 	q = Queries('testDB')
 	q.update('tableB',{'first':'macers','last':'kitters'},{'num':12345})
